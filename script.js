ee46a039b7933c76cf83c0da932f08bc49bfbdd3
Pavel Komarov <komarov.pa.it@gmail.com>
2020-11-01 02:32:22 +0300

    Adds counting functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const SPEED = 500;
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText
+
+    const inc = target / SPEED;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  }
+
+  updateCount();
+})
